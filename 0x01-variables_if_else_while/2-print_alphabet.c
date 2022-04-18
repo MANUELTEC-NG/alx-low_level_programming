29bb2f737fd0d04762d2e37d56d7d7cd34b53a56
manueltec-ng <manueltech401@gmail.com>
2022-04-18 08:26:17 +0100

    Add file "2-print_alphabet.c"

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - prints lowercase alphabet a-z
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char a = 'a';
+
+while(a <= 'z')
+{
+putchar(a);
+a++;
+putchar('\n');
+
+}
+}
