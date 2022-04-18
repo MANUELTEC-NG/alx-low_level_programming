6262744d8d3a5fa5f530edc84e5d7ba6da200ccf
manueltec-ng <manueltech401@gmail.com>
2022-04-18 12:22:26 +0100

    Add file "5-print_numbers.c"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints single digit 0-9
+ * of the base 10 number system
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int deci_digit;
+deci_digit = 48;
+
+while (deci_digit != 58)
+{
+putchar(deci_digit);
+deci_digit++;
+
+}
+return (0);
+
+}
