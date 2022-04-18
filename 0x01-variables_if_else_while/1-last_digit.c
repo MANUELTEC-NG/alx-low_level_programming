9435930b62ca5d2996c72c629205b692c9b1f16d
manueltec-ng <manueltech401@gmail.com>
2022-04-18 06:19:52 +0100

    Add file '1-last_digit.c'

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else if (n < 6 && n != 0)
 {
-printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 
 }
 
