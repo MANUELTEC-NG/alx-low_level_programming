2ebcd77f7833e46a36d1ded703164800c23e282d
manueltec-ng <manueltech401@gmail.com>
2022-06-11 07:54:23 +0100

    add object file and libmy.a file

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -2,9 +2,9 @@
 
 /**
  * _atoi - convert a string to an integer.
- * @s: the string to be comverted.
+ * @s: the string to be converted.
  *
- *Return: The integer value of the comverted string.
+ *Return: The integer value of the converted string.
  */
 
 int _atoi(char *s)
