9da02aa451f5beafa7ac14f5172d2474c80d6f9f
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-06-16 18:06:05 +0100

    Modify for Betty Doc Style Requirement

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,15 +5,17 @@
  * @name: Name of the dog
  * @age: age of the dog.
  * @owner: owner of the dog.
- * Desctiption: a structure with the name, age and owner of a dog.
-**/
+ * Description: a structure with the name, age and owner of a dog.
+*/
 
 typedef struct dog
 {
     char *name;
-    char *owner;
     float age;
+    char *owner;
+
 } dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
